7862756f9257a93a7ba48fd6f80b41899c2de95e
Derinko <derickpaula2@gmail.com>
2020-09-06 17:35:36 -0400

    s

--- js/rest.js
+++ js/rest.js
@@ -1,14 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const timeLeftDisplay = document.querySelector('#time-left')
     const startBtn = document.querySelector("#start")
-    let timeleft = 10;
+    let timeleft = 3;
 
     function countDown() {
         setInterval(function () {
             if (timeleft <= 0) {
                 clearInterval(timeleft = 0)
-                var audio = new Audio('timesup.mp3');
-                audio.play();
+                var audio = new Audio('time.mp3');
+                
+                setTimeout(function(){
+                    audio.play();
+                
+                    setTimeout(function(){
+                        audio.pause();
+                        audio.currentTime = 0;
+                    }, 2000);
+                }, 1000);
             }
             timeLeftDisplay.textContent = "00:" + timeleft;
             timeleft -= 1;
